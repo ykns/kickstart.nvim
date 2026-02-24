ab3a2aae6b069abf6dcf0973f1cb25c3999f21f4
yax <yakeen.sewsanker@gmail.com>
2026-02-24 15:48:26 +0000

    feat: `]t` next tab `[t` prev tab

--- init.lua
+++ init.lua
@@ -194,6 +194,10 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- buffer navigation
 vim.keymap.set('n', ']b', '<cmd>bnext<CR>', { desc = 'Next buffer' })
 vim.keymap.set('n', '[b', '<cmd>bprev<CR>', { desc = 'Prev buffer' })
+
+-- tab navigation
+vim.keymap.set('n', ']t', '<cmd>+tabnext<CR>', { desc = 'Next tab' })
+vim.keymap.set('n', '[t', '<cmd>-tabnext<CR>', { desc = 'Prev tab' })
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
