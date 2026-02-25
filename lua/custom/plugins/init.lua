dd442c8c6619c1740fa44e5f04ccb1d54955c363
yax <yakeen.sewsanker@gmail.com>
2026-02-25 20:48:07 +0000

    fix(nvim): prevent orphan terminals by not saving them in the session on exit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -74,7 +74,7 @@ return {
     'rmagatti/auto-session',
     lazy = false,
     config = function()
-      vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
+      vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,localoptions'
       require('auto-session').setup {
         auto_restore = true,
         auto_save = true,
