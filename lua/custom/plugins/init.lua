4fe87ee63020ff6f2fe5bfb584808ac8b8b49aae
yax <yakeen.sewsanker@gmail.com>
2026-02-25 20:39:01 +0000

    fix(nvim): auto-session setup not being called
    Used `:checkhealth auto-session` to help figure it out

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -73,14 +73,15 @@ return {
   {
     'rmagatti/auto-session',
     lazy = false,
-
-    ---enables autocomplete for opts
-    ---@module "auto-session"
-    ---@type AutoSession.Config
-    opts = {
-      suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
-      -- log_level = 'debug',
-    },
+    config = function()
+      vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
+      require('auto-session').setup {
+        auto_restore = true,
+        auto_save = true,
+        -- log_level = 'debug',
+        suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      }
+    end,
   },
   {
     'pmizio/typescript-tools.nvim',
